0c3a3d2f5fd54aba6336f5a43973992ca6495f1e
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-04-21 19:21:57 -0300

    labels

---
+++ labels.py
@@ -0,0 +1,29 @@
+import d6tflow as d6t
+import pandas as pd
+from tqdm import tqdm
+import socceraction.vaep.labels as lb
+
+from loaders import WyscoutToSPADL
+
+class CreateVAEPLabels(d6t.tasks.TaskCSVPandas):
+    competition_name = d6t.Parameter()
+
+    def requires(self):
+        return WyscoutToSPADL(competition_name=self.competition_name)
+
+    def run(self):
+        actions = self.inputLoad()
+        
+        yfns = [
+            lb.scores,
+            lb.concedes,
+            lb.goal_from_shot
+        ]
+
+        labels = []
+        for game in tqdm(actions.game_id.unique(), desc="Creating labels"):
+            actions_game = actions[actions.game_id==game].reset_index(drop=True)
+            labels.append(pd.concat([fn(actions_game) for fn in yfns], axis=1))
+
+        labels = pd.concat(labels).reset_index(drop=True)
+        self.save(labels)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import d6tflow as d6t
 
 from wyVAEP import wyVAEP
-from loaders import WyscoutToSPADL
 
-wy = WyscoutToSPADL(competition_name='English first division')
+wy = wyVAEP(competition_name='English first division')
 wy.run()
 out = wy.outputLoad()
 
