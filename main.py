cfa529be1f4902574a33d28d21911456e74156af
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-04-27 22:16:35 -0300

    summ table working

--- main.py
+++ main.py
@@ -3,28 +3,33 @@ import pandas as pd
 from features import CreateVAEPFeatures
 from wyVAEP import wyVAEP
 from computeVAEP import ComputeVAEP
+from wyDataLoader import wyLoadTimePlayed
 
+DATA_DIR = 'H:\Documentos\SaLab\Soccermatics\Wyscout Data'
 COMPETITION_NAME = 'English first division'
 TRAIN_COMPETITIONS = ['Spanish first division', 'Italian first division', 'French first division']
+
+pt = d6t.Workflow(wyLoadTimePlayed, params={'data_dir': DATA_DIR})
+pt.run()
+played_time = pt.outputLoad()
+minutes_table = played_time.groupby('player_name')['minutes_played'].sum().reset_index(drop=False)
+minutes_table = minutes_table.rename(columns={'sum': 'minutes_played'})
 
 wy3 = d6t.Workflow(wyVAEP, params={'competition_name': COMPETITION_NAME, 'num_prev_actions': 3})
 wy3.run()
 VAEP3actions = wy3.outputLoad()
-wy5 = d6t.Workflow(wyVAEP, params={'competition_name': COMPETITION_NAME, 'num_prev_actions': 5})
-wy5.run()
-VAEP5actions = wy5.outputLoad()
-wy10 = d6t.Workflow(wyVAEP, params={'competition_name': COMPETITION_NAME, 'num_prev_actions': 10})
-wy10.run()
-VAEP10actions = wy10.outputLoad()
 
+vaep_table = VAEP3actions.groupby('player_name')['VAEP'].agg(['count','sum']).reset_index(drop=False)
+vaep_table = vaep_table.rename(columns={'sum': 'VAEP'})
+scores_table = VAEP3actions.groupby('player_name')['scores'].sum().reset_index(drop=False)
+scores_table = scores_table.rename(columns={'sum': 'scores'})
+concedes_table = VAEP3actions.groupby('player_name')['concedes'].sum().reset_index(drop=False)
+concedes_table = concedes_table.rename(columns={'sum': 'concedes'})
 
+summ_table = vaep_table.merge(scores_table).merge(concedes_table).merge(minutes_table).reset_index(drop=True)
 
-'''
-minutes = pd.read_json("H:\Documentos\SaLab\Soccermatics\Wyscout Data\minutes_played_per_game_England.json",encoding='raw_unicode_escape')
-playing_time = minutes.rename(columns={'playerId': 'player_id'})
-playing_time = playing_time.groupby('player_id')['minutesPlayed'].sum().reset_index()
-vaep_by_player = VAEPactions.groupby('player_id')['VAEP'].agg(['count','mean','sum','max','min']).sort_values(by='mean', ascending=False).reset_index(drop=False)
-vaep_by_player = vaep_by_player.merge(playing_time, on='player_id')
-vaep_by_player['rating'] = (vaep_by_player['sum']*90) / vaep_by_player['minutesPlayed']
-vaep_by_player = vaep_by_player.loc[vaep_by_player.minutesPlayed>450].sort_values(by='rating', ascending=False)
-'''
+summ_table['rating'] = (summ_table['VAEP']*90) / summ_table['minutes_played']
+summ_table['offensive'] = (summ_table['scores']*90) / summ_table['minutes_played']
+summ_table['defensive'] = (summ_table['concedes']*90) / summ_table['minutes_played']
+summ_table = summ_table.sort_values(by=['rating','offensive','defensive'], ascending=[False,False,True])
+summ_table = summ_table.loc[summ_table.minutes_played>450]
--- wyDataLoader.py
+++ wyDataLoader.py
@@ -1,21 +1,32 @@
 import pandas as pd
 import d6tflow as d6t
 
+
+
 class wyLoadData(d6t.tasks.TaskCSVPandas):
     data_dir = d6t.Parameter()
     
     def run(self):
         players = pd.read_json(path_or_buf=self.data_dir+'\players.json')
         teams = pd.read_json(path_or_buf=self.data_dir+'\\teams.json')
-        minutes_played_england = pd.read_json(path_or_buf=self.data_dir+'\minutes_played_per_game_England.json')
+        #minutes_played_england = pd.read_json(path_or_buf=self.data_dir+'\minutes_played_per_game_England.json')
 
         players = players.rename(columns={'wyId': 'player_id', 'shortName': 'player_name', 'currentTeamId': 'team_id'})
         teams = teams.rename(columns={'wyId': 'team_id', 'name': 'team_name'})
-        minutes_played_england = minutes_played_england.rename(columns={'playerId': 'player_id', 'minutesPlayed':'minutes_played','matchId':'game_id'})
+        #minutes_played_england = minutes_played_england.rename(columns={'playerId': 'player_id', 'minutesPlayed':'minutes_played','matchId':'game_id'})
         players = players[['player_id', 'player_name', 'team_id']]
         teams = teams[['team_id', 'team_name']]
-        minutes_played_england = minutes_played_england[['player_id', 'minutes_played','game_id']]
+        #minutes_played_england = minutes_played_england[['player_id', 'minutes_played','game_id']]
 
-        wyData = players.merge(teams, on='team_id').merge(minutes_played_england, on='player_id')
+        wyData = players.merge(teams, on='team_id')
 
         self.save(wyData)
+
+class wyLoadTimePlayed(d6t.tasks.TaskCSVPandas):
+    data_dir = d6t.Parameter()
+
+    def run(self):
+        minutes_played_england = pd.read_json(path_or_buf=self.data_dir+'\minutes_played_per_game_England.json')
+        minutes_played_england = minutes_played_england.rename(columns={'playerId': 'player_id', 'minutesPlayed':'minutes_played','matchId':'game_id','shortName':'player_name'})
+        minutes_played_england = minutes_played_england[['player_id', 'minutes_played','game_id','player_name']]
+        self.save(minutes_played_england)
