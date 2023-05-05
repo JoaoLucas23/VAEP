0fc88914008666313fdfc7ba4d3fac6aee906b3d
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-05-05 17:22:25 -0300

    vaep rodando com treino alemanha,franca,italia

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from wyDataLoader import wyLoadTimePlayed
 from functions import *
 
 DATA_DIR = 'H:\Documentos\SaLab\Soccermatics\Wyscout Data'
-COMPETITION_NAME = 'English first division'
-TRAIN_COMPETITIONS = ['Spanish first division', 'Italian first division', 'French first division']
+ENGLAND = 'English first division'
+SPAIN = 'Spanish first division'
+TRAIN_COMPETITIONS = ['German first division', 'Italian first division', 'French first division']
 
 pt = d6t.Workflow(wyLoadTimePlayed, params={'data_dir': DATA_DIR})
 pt.run()
@@ -17,13 +18,17 @@ played_time = pt.outputLoad()
 minutes_table = played_time.groupby('player_name')['minutes_played'].sum().reset_index(drop=False)
 minutes_table = minutes_table.rename(columns={'sum': 'minutes_played'})
 
-wy3 = d6t.Workflow(wyVAEP, params={'competition_name': COMPETITION_NAME, 'num_prev_actions': 3})
-wy3.run()
-VAEP3actions = wy3.outputLoad()
+en = d6t.Workflow(wyVAEP, params={'competition_name': ENGLAND, 'num_prev_actions': 3, 'train_competitions': TRAIN_COMPETITIONS})
+en.run()
+englandVAEP = en.outputLoad()
 
-player_summ_table = oneColumnGroupedVAEP(VAEP3actions,column='player_name',by90=True,minutes_table=minutes_table)
-action_summ_table = oneColumnGroupedVAEP(df=VAEP3actions, column='type_name',by90=False)
-result_summ_table = oneColumnGroupedVAEP(df=VAEP3actions, column='result_name',by90=False)
+sp = d6t.Workflow(wyVAEP, params={'competition_name': SPAIN, 'num_prev_actions': 3, 'train_competitions': TRAIN_COMPETITIONS})
+sp.run()
+spainVAEP = sp.outputLoad()
 
-player_game_table = multipleColumnsGroupedVAEP(df=VAEP3actions,columns=['player_name','game_id'])
-player_action_table = multipleColumnsGroupedVAEP(df=VAEP3actions,columns=['player_name','type_name'])
+player_summ_table = oneColumnGroupedVAEP(englandVAEP,column='player_name',by90=True,minutes_table=minutes_table)
+action_summ_table = oneColumnGroupedVAEP(df=englandVAEP, column='type_name',by90=False)
+result_summ_table = oneColumnGroupedVAEP(df=englandVAEP, column='result_name',by90=False)
+
+player_game_table = multipleColumnsGroupedVAEP(df=englandVAEP,columns=['player_name','game_id'])
+player_action_table = multipleColumnsGroupedVAEP(df=englandVAEP,columns=['player_name','type_name'])
--- wyDataLoader.py
+++ wyDataLoader.py
@@ -9,15 +9,10 @@ class wyLoadData(d6t.tasks.TaskCSVPandas):
     def run(self):
         players = pd.read_json(path_or_buf=self.data_dir+'\players.json')
         teams = pd.read_json(path_or_buf=self.data_dir+'\\teams.json')
-        #minutes_played_england = pd.read_json(path_or_buf=self.data_dir+'\minutes_played_per_game_England.json')
-
         players = players.rename(columns={'wyId': 'player_id', 'shortName': 'player_name', 'currentTeamId': 'team_id'})
         teams = teams.rename(columns={'wyId': 'team_id', 'name': 'team_name'})
-        #minutes_played_england = minutes_played_england.rename(columns={'playerId': 'player_id', 'minutesPlayed':'minutes_played','matchId':'game_id'})
         players = players[['player_id', 'player_name', 'team_id']]
         teams = teams[['team_id', 'team_name']]
-        #minutes_played_england = minutes_played_england[['player_id', 'minutes_played','game_id']]
-
         wyData = players.merge(teams, on='team_id')
 
         self.save(wyData)
